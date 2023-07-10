a19cd52f64356a32c644b633f8a1587c6a8a9a86
Ziyodullo Aliyev <89968839+Ziyodullodev@users.noreply.github.com>
2023-07-11 01:00:08 +0500

    add: some function and make bot

--- function.php
+++ function.php
@@ -2,7 +2,38 @@
 
 $config = require_once "config.php";
 
+function bot($method,$datas=[]){
+    global $config;
+    $url = "https://api.telegram.org/bot".$config['token']."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
 
-var_dump($config);
+function sendMessage($chat_id, $text, $keys, $c)
+{
+    bot('sendmessage', [
+        'chat_id' => $chat_id,
+        'text' => $text,
+        'reply_markup' => MakeKey($keys, $c)
+    ]);
+}
 
 
+/// create keyboard
+function MakeKey($data = array(),$c = 2){
+    $i = 0;
+    foreach($data as $key=>$v){
+        $keytype=['text'=>$v];
+        $k[floor($i/$c)][$i%$c]=$keytype;
+        $i++;
+    }
+    return json_encode(array('keyboard'=>$k,'resize_keyboard'=>true));
+  }
---
+++ hook.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "function.php";
+
+
+$update = json_decode(file_get_contents('php://input'));
+
+$main_keys = [
+    "Picture",
+    "Music", 
+    "Information"
+];
+
+$pic_keys = [
+    "ism1",
+    "ism2",
+    "back"
+];
+
+
+$text = $update->message->text;
+if ($text == "/start") {
+    sendMessage($update->message->chat->id, "Hello welcome to ".$config['bot_name']. " 😊",$main_keys, 2);
+
+}elseif ($text == "Picture") {
+
+    sendMessage($update->message->chat->id, "rasm", $pic_keys, 4);
+}elseif ($text == "back") {
+
+    sendMessage($update->message->chat->id, "How can i help you ?", $main_keys, 2);
+} else {
+    sendMessage($update->message->chat->id, "🤷‍♀️ I don't know this command", $main_keys, 2);
+}
